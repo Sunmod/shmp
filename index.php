2f2bc87a2856ad05fa5a5c236ea8e617b63a55b7
Stepan <kaskader2001@mail.ru>
2023-10-24 11:39:02 +0300

    инициализировал посадку на WP

--- index.php
+++ index.php
@@ -1,3 +1,164 @@
-<?php get_header(); ?>   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+      <header class="header">
+         <div class="top">
+            <a class="logo" href="#" style="display: block;"></a>
+            <div class="partners"></div>
+            <a class="phone" href="tel:88007070711">8 (800) <b>707-07-11</b> </a>
+         </div>
+         <div class="bottom">
 
-<?php get_footer(); ?>
+            <div class="burger">
+               <span></span>
+            </div>
+   
+            <nav class="navigation">
+               <a class="link" href="">Главная</a>
+               <a class="link" href="">Расписание</a>
+               <a class="link" href="">Монитор</a>
+               <a class="link" href="">Дополнительные материалы</a>
+               <a class="link" href="">Поддержка</a>
+            </nav>
+
+            <nav class="navigation mobile">
+               <a class="link" href="">Главная</a>
+               <a class="link" href="">Расписание</a>
+               <a class="link" href="">Монитор</a>
+               <a class="link" href="">Дополнительные материалы</a>
+               <a class="link" href="">Поддержка</a>
+               <a class="manual--mobile" href="">Инструкция к обучению</a>
+            </nav>
+            <a class="manual mobile" href="">Инструкция к обучению</a>
+            <a class="manual" href="">Инструкция к обучению</a>
+         </div>
+      </header>
+
+      <section class="banner__container">
+         <img class="banner" src="./assets/images/banner1.png" alt="">
+      </section>
+
+      <section class="container__block">
+         <p class="bold orange center" style="margin-bottom: 20px;">Уважаемые участники в декабре состоится Конкурс на лучший бизнес-проект, который<br>
+            предполагает возможность получить 1 млн. руб. Уже сейчас Вы можете ознакомиться с Положением<br>
+            о Конкурсе, графиком проведения и необходимыми документами для участия.</p>
+         <p class="bold center">Мы размещаем необходимую информацию заранее, чтобы у Вас было достаточно<br> времени для подготовки Вашего проекта.</p>
+      </section>
+
+      <section class="container__block">
+         <h2 class="title">График проведения конкурса</h2>
+         <div class="calendar__list">
+            <div class="calendar__item">
+               <div class="calendar__item-box">
+                  <span>до 8</span>
+                  <span>Декабря</span>
+               </div>
+               <p class="calendar__item-description">Подготовка<br> проекта<br> к конкурсу</p>
+            </div>
+
+            <div class="calendar__item">
+               <div class="calendar__item-box">
+                  <span>8-10</span>
+                  <span>Декабря</span>
+               </div>
+               <p class="calendar__item-description">Заявка<br> на участие,<br> публикация<br> проекта</p>
+            </div>
+
+            <div class="calendar__item">
+               <div class="calendar__item-box">
+                  <span>10-13</span>
+                  <span>Декабря</span>
+               </div>
+               <p class="calendar__item-description">Модерация проектов</p>
+            </div>
+
+            <div class="calendar__item">
+               <div class="calendar__item-box">
+                  <span>14-17</span>
+                  <span>Декабря</span>
+               </div>
+               <p class="calendar__item-description">Голосование, открытие<br> биржи проектов</p>
+            </div>
+
+            <div class="calendar__item event">
+               <div class="calendar__item-box">
+                  <span>20</span>
+                  <span>Декабря</span>
+               </div>
+               <p class="calendar__item-description">Оглашение финальных результатов</p>
+            </div>
+         </div>
+      </section>
+
+      <section class="container__block">
+         <div class="condition__button-container">
+            <a class="condition__button" href="">Скачать положения конкурса</a>
+         </div>
+      </section>
+
+      <section class="container__block">
+         <h2 class="title">Видеоролик презентации Вашего проекта должен быть не более 2 - 2:30 минут и содержательно должен отражать:</h2>
+         <div class="steps__list">
+            <div class="step__item">
+               <p class="step__item-number">1</p>
+               <p class="step__item-description">Короткую информацию о Вас, как о предпринимателе, и понятное описание проекта (в чем его суть, что является продуктом (товар, работа, услуга), какую проблему или потребность закрывает Ваш продукт для клиента (30–40 сек)*.</p>
+            </div>
+            <div class="step__item">
+               <p class="step__item-number">2</p>
+               <p class="step__item-description">Описание текущего этапа развития проекта: что сделано на сегодняшний день, кто ключевые клиенты, какие достигнуты результаты (тесты, продажи, оборот и т. д. (любые показатели, которые считаете значимыми)) (30 сек)*</p>
+            </div>
+            <div class="step__item">
+               <p class="step__item-number">3</p>
+               <p class="step__item-description">Информация о сделанных инвестициях (если есть), о планируемых инвестициях (необходимый объем финансирования). Цели, на которые планируете потратить инвестиции. Если инвестиции не требуются, коротко опишите почему. (20 сек)*</p>
+            </div>
+            <div class="step__item">
+               <p class="step__item-number">4</p>
+               <p class="step__item-description">Опишите Ваши первые шаги, которые начнете делать «уже завтра», что по Вашему мнению нужно делать именно сейчас для достижения успеха. (30 сек)*</p>
+            </div>
+            <div class="step__item">
+               <p class="step__item-number">5</p>
+               <p class="step__item-description">Одним предложением опишите, чем в наибольшей степени Вам помог Проект «Школа молодого предпринимателя. Бизнес молодых» (10 сек)*</p>
+            </div>
+            <div class="step__item">
+               <p class="step__item-description orange">*Указанный тайминг носит рекомендательный характер и может отличаться в Ваших видеоматериалах.</p>
+               <p class="step__item-description bold"><span class="orange bold">ВАЖНО:</span>вместе с видеороликом презентации Вам необходимо будет загрузить на платформу презентацию Вашего проекта, которую Вы защищаете на Конкурсе бизнес-проектов, в формате pdf.</p>
+            </div>
+         </div>
+      </section>
+
+      <footer class="footer">
+         <div class="top">
+            <nav class="navigation">
+               <a class="link" href="">Главная</a>
+               <a class="link" href="">Расписание</a>
+               <a class="link" href="">Монитор</a>
+               <a class="link" href="">Дополнительные материалы</a>
+               <a class="link" href="">Поддержка</a>
+            </nav>
+         </div>
+         <div class="bottom">
+            <a class="logo" href="#" style="display: block;"></a>
+            <div class="partners"></div>
+            <a class="phone" href="tel:88007070711">8 (800) <b>707-07-11</b> </a>
+         </div>
+      </footer>
+
+
+    </div>
+<script>
+   document.querySelector('.burger').addEventListener('click', function() {
+      this.classList.toggle('active');
+      document.querySelector('.navigation.mobile').classList.toggle('open');
+      document.querySelector('.manual.mobile').classList.toggle('open');
+      document.querySelector('.bottom').classList.toggle('open');
+   })
+</script>
+</body>
+</html>
