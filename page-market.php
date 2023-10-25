c29994e013c041f1af7927d50e143e2c43aff66a
Stepan <kaskader2001@mail.ru>
2023-10-25 15:51:52 +0300

    создал страницу поста, теперь можно создавать посты и с биржи переходить к их описанию

--- page-market.php
+++ page-market.php
@@ -29,12 +29,27 @@
       </section>
       <section>
          <div class="market__list">
-            <div class="market__list-item">
+            <?php
+                global $post;
+
+                $myposts = get_posts([ 
+                    'numberposts' => -1,
+                    'post_type'   => 'project',
+                    'order'       => 'ASC'
+                ]);
+
+                if( $myposts ){
+                    foreach( $myposts as $post ){
+                setup_postdata( $post );
+            ?>
+               <div class="market__list-item">
                <div class="market__item-content">
-                  <img class="market__item-img" src="./assets/images/post1.jpeg" alt="">
-                  <p class="market__item-company">OOO «Рога и Копыта»</p>
-                  <p class="market__item-industry">Отрасль: Туризм и образование</p>
-                  <p class="market__item-description">Прекрасное краткое описание на парочку строк, рекрасное краткое описание на парочку строк.</p>
+                  <a href="<?php echo get_permalink(); ?>">
+                     <?php the_post_thumbnail('thumbnail', array('class' => 'market__item-img')); ?>
+                  </a>
+                  <div class="market__item-company"><?php the_title(); ?></div>
+                  <div class="market__item-industry"><?php the_field('project_industry'); ?></div>
+                  <?php the_content(); ?>
                   <div class="line"></div>
                   <div class="market__item-investment">
                      <div>Проинвестировано:</div><div>300 000 ₽</div>
@@ -49,126 +64,14 @@
                   <button class="market__doaction-button">ок</button>
                </div>
             </div>
-            <div class="market__list-item">
-               <div class="market__item-content">
-                  <img class="market__item-img" src="./assets/images/post1.jpeg" alt="">
-                  <p class="market__item-company">OOO «Рога и Копыта»</p>
-                  <p class="market__item-industry">Отрасль: Туризм и образование</p>
-                  <p class="market__item-description">Прекрасное краткое описание на парочку строк, рекрасное краткое описание на парочку строк.</p>
-                  <div class="line"></div>
-                  <div class="market__item-investment">
-                     <div>Проинвестировано:</div><div>300 000 ₽</div>
-                  </div>
-                  <div class="market__item-need">
-                     <div>Требуется инвестиций:</div><div>3 000 000 ₽</div>
-                  </div>
-               </div>
-               <div class="market__doaction-block">
-                  <span>Проинвестировать:</span>
-                  <input class="market__doaction-input" type="text" placeholder="баллов">
-                  <button class="market__doaction-button">ок</button>
-               </div>
-            </div>
-            <div class="market__list-item">
-               <div class="market__item-content">
-                  <img class="market__item-img" src="./assets/images/post1.jpeg" alt="">
-                  <p class="market__item-company">OOO «Рога и Копыта»</p>
-                  <p class="market__item-industry">Отрасль: Туризм и образование</p>
-                  <p class="market__item-description">Прекрасное краткое описание на парочку строк, рекрасное краткое описание на парочку строк.</p>
-                  <div class="line"></div>
-                  <div class="market__item-investment">
-                     <div>Проинвестировано:</div><div>300 000 ₽</div>
-                  </div>
-                  <div class="market__item-need">
-                     <div>Требуется инвестиций:</div><div>3 000 000 ₽</div>
-                  </div>
-               </div>
-               <div class="market__doaction-block">
-                  <span>Проинвестировать:</span>
-                  <input class="market__doaction-input" type="text" placeholder="баллов">
-                  <button class="market__doaction-button">ок</button>
-               </div>
-            </div>
-            <div class="market__list-item">
-               <div class="market__item-content">
-                  <img class="market__item-img" src="./assets/images/post1.jpeg" alt="">
-                  <p class="market__item-company">OOO «Рога и Копыта»</p>
-                  <p class="market__item-industry">Отрасль: Туризм и образование</p>
-                  <p class="market__item-description">Прекрасное краткое описание на парочку строк, рекрасное краткое описание на парочку строк.</p>
-                  <div class="line"></div>
-                  <div class="market__item-investment">
-                     <div>Проинвестировано:</div><div>300 000 ₽</div>
-                  </div>
-                  <div class="market__item-need">
-                     <div>Требуется инвестиций:</div><div>3 000 000 ₽</div>
-                  </div>
-               </div>
-               <div class="market__doaction-block">
-                  <span>Проинвестировать:</span>
-                  <input class="market__doaction-input" type="text" placeholder="баллов">
-                  <button class="market__doaction-button">ок</button>
-               </div>
-            </div>
-            <div class="market__list-item">
-               <div class="market__item-content">
-                  <img class="market__item-img" src="./assets/images/post1.jpeg" alt="">
-                  <p class="market__item-company">OOO «Рога и Копыта»</p>
-                  <p class="market__item-industry">Отрасль: Туризм и образование</p>
-                  <p class="market__item-description">Прекрасное краткое описание на парочку строк, рекрасное краткое описание на парочку строк.</p>
-                  <div class="line"></div>
-                  <div class="market__item-investment">
-                     <div>Проинвестировано:</div><div>300 000 ₽</div>
-                  </div>
-                  <div class="market__item-need">
-                     <div>Требуется инвестиций:</div><div>3 000 000 ₽</div>
-                  </div>
-               </div>
-               <div class="market__doaction-block">
-                  <span>Проинвестировать:</span>
-                  <input class="market__doaction-input" type="text" placeholder="баллов">
-                  <button class="market__doaction-button">ок</button>
-               </div>
-            </div>
-            <div class="market__list-item">
-               <div class="market__item-content">
-                  <img class="market__item-img" src="./assets/images/post1.jpeg" alt="">
-                  <p class="market__item-company">OOO «Рога и Копыта»</p>
-                  <p class="market__item-industry">Отрасль: Туризм и образование</p>
-                  <p class="market__item-description">Прекрасное краткое описание на парочку строк, рекрасное краткое описание на парочку строк.</p>
-                  <div class="line"></div>
-                  <div class="market__item-investment">
-                     <div>Проинвестировано:</div><div>300 000 ₽</div>
-                  </div>
-                  <div class="market__item-need">
-                     <div>Требуется инвестиций:</div><div>3 000 000 ₽</div>
-                  </div>
-               </div>
-               <div class="market__doaction-block">
-                  <span>Проинвестировать:</span>
-                  <input class="market__doaction-input" type="text" placeholder="баллов">
-                  <button class="market__doaction-button">ок</button>
-               </div>
-            </div>
-            <div class="market__list-item">
-               <div class="market__item-content">
-                  <img class="market__item-img" src="./assets/images/post1.jpeg" alt="">
-                  <p class="market__item-company">OOO «Рога и Копыта»</p>
-                  <p class="market__item-industry">Отрасль: Туризм и образование</p>
-                  <p class="market__item-description">Прекрасное краткое описание на парочку строк, рекрасное краткое описание на парочку строк.</p>
-                  <div class="line"></div>
-                  <div class="market__item-investment">
-                     <div>Проинвестировано:</div><div>300 000 ₽</div>
-                  </div>
-                  <div class="market__item-need">
-                     <div>Требуется инвестиций:</div><div>3 000 000 ₽</div>
-                  </div>
-               </div>
-               <div class="market__doaction-block">
-                  <span>Проинвестировать:</span>
-                  <input class="market__doaction-input" type="text" placeholder="баллов">
-                  <button class="market__doaction-button">ок</button>
-               </div>
-            </div>
+            <?php 
+                        }
+                } else {
+                    
+                }
+
+                wp_reset_postdata();
+            ?>
          </div>
       </section>
    </div>
--- page-market_post.php
+++ page-market_post.php
@@ -1,8 +1,14 @@
 <?php
 // Template Name: Биржа пост
+// Template Post Type: project, pages
 ?>
 <?php get_header(); ?>
 <section>
-    111
+<?php the_post_thumbnail('thumbnail', array('class' => 'market__item-img')); ?>
+<?php the_title(); ?>
+<?php the_field('project_industry'); ?>
+<?php the_content(); ?>
+<?php the_field('project_title'); ?>
+<?php the_field('project_description'); ?>
 </section>
 <?php get_footer() ?>
