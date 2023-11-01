aad51f86f6a1e4ee294ef243312bfc60b0f2f420
Stepan <kaskader2001@mail.ru>
2023-11-01 17:50:44 +0300

    разобрался почему календари не подключались. js код не срабатывал - починил (костыль)

--- functions.php
+++ functions.php
@@ -5,13 +5,23 @@
 add_action('wp_enqueue_scripts' , 'script_styles');
 function script_styles() {
     wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/style.css' );
-    wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.js','','',true);
+    wp_enqueue_style('style', get_template_directory_uri() . '/assets/css/calendar.css' );
 }
 
 add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
 add_theme_support('custom-logo');
 add_theme_support( 'menus' );
+
+
+function calendars() {  
+    wp_register_script( 'calendar1', get_template_directory_uri() . '/script.js',  array(), '1' );
+    wp_register_script( 'calendar2', get_template_directory_uri() . '/index.global.js',  array(), '1' );  
+
+    wp_enqueue_script( 'calendar1' );
+    wp_enqueue_script( 'calendar2' );
+}  
+add_action( 'wp_enqueue_scripts', 'calendars' ); 
 
 
 // скрипт добавляет возможность загружать свг логотипы в WP
@@ -43,64 +53,66 @@ function fix_svg_mime_type($data,$file,$filename,$mimes,$real_mime=''){
 
 add_filter( 'show_admin_bar', '__return_true' );
 
-function event_date($date){
-    $new_date = getdate(strtotime($date));
-    $my_months=[
-        'January'   => 'Январе',
-        'February'  => 'Феврале',
-        'March'     => 'Марте',
-        'April'     => 'Апреле',
-        'May'       => 'Мае',
-        'June'      => 'Июне',
-        'July'      => 'Июле',
-        'August'    => 'Августе',
-        'September' => 'Сентябре',
-        'October'   => 'Октябре',
-        'November'  => 'Ноябре',
-        'December'  => 'Декабре',
-    ];
+// Это код календаря с кубсайнц. Пока не понимаю как его использовать
 
-    $new_date['month']= $my_months[$new_date['month']];
+// function event_date($date){
+//     $new_date = getdate(strtotime($date));
+//     $my_months=[
+//         'January'   => 'Январе',
+//         'February'  => 'Феврале',
+//         'March'     => 'Марте',
+//         'April'     => 'Апреле',
+//         'May'       => 'Мае',
+//         'June'      => 'Июне',
+//         'July'      => 'Июле',
+//         'August'    => 'Августе',
+//         'September' => 'Сентябре',
+//         'October'   => 'Октябре',
+//         'November'  => 'Ноябре',
+//         'December'  => 'Декабре',
+//     ];
 
-    return $new_date['month'];
-}
-function event_date_single($date){
-    $new_date = getdate(strtotime($date));
-    $my_months=[
-        'January'   => 'Января',
-        'February'  => 'Февраля',
-        'March'     => 'Марта',
-        'April'     => 'Апреля',
-        'May'       => 'Мая',
-        'June'      => 'Июня',
-        'July'      => 'Июля',
-        'August'    => 'Августа',
-        'September' => 'Сентября',
-        'October'   => 'Октября',
-        'November'  => 'Ноября',
-        'December'  => 'Декабря',
-    ];
+//     $new_date['month']= $my_months[$new_date['month']];
 
-    $new_date['month']= $my_months[$new_date['month']];
+//     return $new_date['month'];
+// }
+// function event_date_single($date){
+//     $new_date = getdate(strtotime($date));
+//     $my_months=[
+//         'January'   => 'Января',
+//         'February'  => 'Февраля',
+//         'March'     => 'Марта',
+//         'April'     => 'Апреля',
+//         'May'       => 'Мая',
+//         'June'      => 'Июня',
+//         'July'      => 'Июля',
+//         'August'    => 'Августа',
+//         'September' => 'Сентября',
+//         'October'   => 'Октября',
+//         'November'  => 'Ноября',
+//         'December'  => 'Декабря',
+//     ];
 
-    return $new_date['month'];
-}
+//     $new_date['month']= $my_months[$new_date['month']];
 
-function num_decline($number, $titles, $show_number = true)
-{
-    if (is_string($titles)) {
-        $titles = preg_split('/, */', $titles);
-    }
-    if (empty($titles[2])) {
-        $titles[2] = $titles[1];
-    }
-    $cases = [2, 0, 1, 1, 1, 2];
-    $intnum = abs((int)strip_tags($number));
-    $title_index = ($intnum % 100 > 4 && $intnum % 100 < 20)
-        ? 2
-        : $cases[min($intnum % 10, 5)];
-    return ($show_number ? "$number " : '') . $titles[$title_index];
-}
+//     return $new_date['month'];
+// }
+
+// function num_decline($number, $titles, $show_number = true)
+// {
+//     if (is_string($titles)) {
+//         $titles = preg_split('/, */', $titles);
+//     }
+//     if (empty($titles[2])) {
+//         $titles[2] = $titles[1];
+//     }
+//     $cases = [2, 0, 1, 1, 1, 2];
+//     $intnum = abs((int)strip_tags($number));
+//     $title_index = ($intnum % 100 > 4 && $intnum % 100 < 20)
+//         ? 2
+//         : $cases[min($intnum % 10, 5)];
+//     return ($show_number ? "$number " : '') . $titles[$title_index];
+// }
 
 function get_user_fio() {
     $current_user = wp_get_current_user();
