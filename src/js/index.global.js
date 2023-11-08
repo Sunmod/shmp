2a3c9b85f8225dfa2dd74a3ae90b46e71ec52ad0
Stepan <kaskader2001@mail.ru>
2023-11-08 11:45:28 +0300

    init webpack + commit

--- src/js/index.global.js
+++ src/js/index.global.js
@@ -3,6 +3,46 @@ FullCalendar Standard Bundle v6.1.9
 Docs & License: https://fullcalendar.io/docs/initialize-globals
 (c) 2023 Adam Shaw
 */
+
+// !!!!!!!!!!!!!!!
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var calendarEl = document.getElementById('calendar');
+    
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      locale: 'ru',
+      firstDay: '1',
+      disableDragging: true,
+      headerToolbar: {
+        left: 'prev,next',
+        center: 'title',
+        // right: 'multiMonthYear,dayGridMonth,timeGridWeek'
+        right: 'today'
+      },
+      initialView: 'dayGridMonth',
+      editable: false,
+      selectable: false,
+      showNonCurrentDates: true,
+      businessHours: true,
+      eventTimeFormat: {
+        hour: 'numeric',
+        minute: '2-digit',
+        meridiem: false,
+      },
+      eventSources: [
+        {
+          events: events
+        }
+      ]
+    });
+    calendar.render();
+  });
+
+// !!!!!!!!!!!!!!!
+
+
+
 var FullCalendar = (function (exports) {
     'use strict';
 
